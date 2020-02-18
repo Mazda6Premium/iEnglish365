e274b0165a9922723676f9c67e6e884fd0ea5b1e
viet1301 <nguyenviet1301@gmail.com>
2020-02-18 10:37:19 +0700

    Initial Commit

---
+++ LearningLanguage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LearningLanguage
+//
+//  Created by Linh Nguyen on 2/18/20.
+//  Copyright © 2020 NguyenQuocViet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
